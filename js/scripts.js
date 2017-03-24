69caeb6f823e2962640b09a4fa2e456b84e77237
Mariusz Kicinski <mariusz.kicinski@gmail.com>
2017-03-24 12:13:24 +0100

    done but not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 (function ($) {
 	$(document).ready(function(){
-		var clicked = false;
+		var clickedChecker = false;
 		$("#checker").on("click", function(){
 			
-			if (!clicked) {
-				$('body').append("<div>button working</div>");		
-				clicked=true;
+			if (!clickedChecker) {
+				if (typeof String.prototype.repeat == "function") {
+					$('body').append("<div>repeat available</div>");		
+				}
+				else {
+					$('body').append("<div>repeat unavailable</div>");			
+				}
+					$('body').append("<button id='addFunction'>click to add repeatt</div>");		
+				clickedChecker=true;
 			}
-		}) 
+		});
+		$("#addFunction").on("click", function(){
+			if (String.prototype.repeatt =="function") return;
+			else{
+				String.prototype.repeatt == function (count) {
+					if (count<0) {
+						throw new RangeError("Invalid count value");
+					}
+					return(new Array(count+1)).join(this.toString());
+				}
+			}
+
+		}); 
 	});
 })(jQuery);
